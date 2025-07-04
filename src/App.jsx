477017723c575b738cdc96f0356774d1de46439b
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-07-04 16:54:40 +0300

    feat: add layout and lorem text file data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-function App() {
-	return <h1>Lorem</h1>;
-}
+import { useState } from 'react';
+import data from './data.js';
+
+const App = () => {
+	const [count, setCount] = useState(1);
+	const [text, setText] = useState([]);
+
+	return (
+		<section className="section-center">
+			<h4>tired of boring lorem ipsunm?</h4>
+			<form className="lorem-form">
+				<label htmlFor="amount">paragraphs:</label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					min="1"
+					step="1"
+					max="8"
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+				/>
+			</form>
+			<button type="submit" className="btn">
+				Generate
+			</button>
+		</section>
+	);
+};
 
 export default App;
